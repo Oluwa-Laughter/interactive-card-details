63f15898ff9bdf569276bce260416db36bdad18d
Oluwa-Laughter <210806073@live.unilag.edu.ng>
2024-09-30 13:57:49 +0100

    update the form validation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 export default function App() {
+  const [completed, setCompleted] = useState(false);
   const [formData, setFormData] = useState({
     cardName: "",
     cardNumber: "",
@@ -9,18 +10,79 @@ export default function App() {
     cardCvc: "",
   });
 
-  const [completed, setCompleted] = useState(false);
+  const [error, setError] = useState({});
 
-  const handleChange = function (e) {
+  const validateInput = (name, value) => {
+    return name === "cardName"
+      ? value
+        ? null
+        : "Please enter a card name"
+      : name === "cardNumber"
+      ? value
+        ? /^[\d\s]+$/.test(value)
+          ? null
+          : "Wrong format, numbers only"
+        : "Please enter a card number"
+      : name === "cardExpiryMonth"
+      ? value
+        ? parseInt(value) > 12 || parseInt(value) < 1 || !/^\d+$/.test(value)
+          ? "Invalid Month or Year"
+          : null
+        : "Can't be blank"
+      : name === "cardExpiryYear"
+      ? value
+        ? parseInt(value) <
+            parseInt(new Date().getFullYear().toString().slice(-2)) ||
+          !/^\d+$/.test(value)
+          ? "Invalid Month or Year"
+          : null
+        : "Can't be blank"
+      : name === "cardCvc"
+      ? value
+        ? /^\d+$/.test(value)
+          ? null
+          : "Wrong format, numbers only"
+        : "Can't be blank"
+      : null;
+  };
+
+  const handleError = () => {
+    const errors = Object.keys(formData).reduce((acc, key) => {
+      const errorMessage = validateInput(key, formData[key]);
+      if (errorMessage) acc[key] = errorMessage;
+      return acc;
+    }, {});
+
+    const hasErrors = Object.values(errors).some((error) => error !== null);
+
+    if (hasErrors) {
+      setError(errors);
+    } else {
+      setCompleted(true);
+    }
+  };
+
+  const handleChange = (e) => {
     const { name, value } = e.target;
+    const formattedValue =
+      name === "cardNumber"
+        ? value.replace(/\s+/g, "").replace(/(\d{4})(?=\d)/g, "$1 ")
+        : value;
+
     setFormData((prevData) => ({
       ...prevData,
-      [name]: value,
+      [name]: formattedValue,
+    }));
+
+    setError((prevError) => ({
+      ...prevError,
+      [name]: validateInput(name, value),
     }));
   };
-  const handleSubmit = function (e) {
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    setCompleted(true);
+    handleError();
   };
 
   return (
@@ -30,13 +92,12 @@ export default function App() {
           <div className="card-details">
             <img src="./images/card-logo.svg" alt="card-logo" />
             <div className="card-info">
-              <h1 className="card-num">{formData.cardNumber}</h1>
-
+              <h1 className="card-num ">{formData.cardNumber}</h1>
               <div className="owner">
                 <h2 className="card-name">{formData.cardName}</h2>
                 <div className="card-date">
                   <span>{formData.cardExpiryMonth}</span> /{" "}
-                  <span>{formData && formData.cardExpiryYear}</span>
+                  <span>{formData.cardExpiryYear}</span>
                 </div>
               </div>
             </div>
@@ -54,8 +115,8 @@ export default function App() {
           <Form
             handleChange={handleChange}
             handleSubmit={handleSubmit}
-            setCompleted={setCompleted}
             formData={formData}
+            error={error}
           />
         )}
       </main>
@@ -63,7 +124,7 @@ export default function App() {
   );
 }
 
-function Form({ formData, handleChange, handleSubmit, setCompleted }) {
+function Form({ formData, handleChange, handleSubmit, error }) {
   return (
     <form className="form" onSubmit={handleSubmit}>
       <section className="form-section">
@@ -72,10 +133,12 @@ function Form({ formData, handleChange, handleSubmit, setCompleted }) {
           type="text"
           id="name"
           name="cardName"
+          className={error.cardName ? "error" : ""}
           value={formData.cardName}
-          onChange={(e) => handleChange(e)}
+          onChange={handleChange}
           placeholder="e.g Jane Appleseed"
         />
+        {error.cardName && <p className="error-message">{error.cardName}</p>}
       </section>
       <section className="form-section">
         <label htmlFor="card-number">Card Number</label>
@@ -84,34 +147,44 @@ function Form({ formData, handleChange, handleSubmit, setCompleted }) {
           id="card-number"
           name="cardNumber"
           value={formData.cardNumber}
-          onChange={(e) => handleChange(e)}
+          className={error.cardNumber ? "error" : ""}
+          onChange={handleChange}
           placeholder="e.g 1234 5678 1234 5678"
-          maxLength={16}
+          maxLength={19}
         />
+        {error.cardNumber && (
+          <p className="error-message">{error.cardNumber}</p>
+        )}
       </section>
       <section className="form-section card-exp">
         <div className="exp-month">
           <label htmlFor="card-expiry">EXP. DATE (MM/YY)</label>
-
           <span>
             <input
-              type="month"
-              id="card-expiry"
+              type="text"
+              id="card-expiry-month"
               name="cardExpiryMonth"
               value={formData.cardExpiryMonth}
-              onChange={(e) => handleChange(e)}
+              onChange={handleChange}
+              className={error.cardExpiryMonth ? "error" : ""}
               placeholder="MM"
               maxLength={2}
             />
             <input
-              type="year"
-              id="card-expiry"
+              type="text"
+              id="card-expiry-year"
               name="cardExpiryYear"
+              className={error.cardExpiryYear ? "error" : ""}
               value={formData.cardExpiryYear}
-              onChange={(e) => handleChange(e)}
+              onChange={handleChange}
               placeholder="YY"
               maxLength={2}
             />
+            {error.cardExpiryYear ? (
+              <p className="error-message">{error.cardExpiryYear}</p>
+            ) : error.cardExpiryMonth ? (
+              <p className="error-message">{error.cardExpiryMonth}</p>
+            ) : null}
           </span>
         </div>
 
@@ -119,14 +192,15 @@ function Form({ formData, handleChange, handleSubmit, setCompleted }) {
           <label htmlFor="card-cvc">CVC</label>
           <input
             type="tel"
-            className="card_cvc"
             id="card-cvc"
             name="cardCvc"
+            className={error.cardCvc ? "card_cvc error" : "card_cvc"}
             value={formData.cardCvc}
-            onChange={(e) => handleChange(e)}
+            onChange={handleChange}
             placeholder="e.g 123"
             maxLength={3}
           />
+          {error.cardCvc && <p className="error-message">{error.cardCvc}</p>}
         </div>
       </section>
       <button type="submit">Confirm</button>
@@ -140,7 +214,9 @@ function ThankYou() {
       <img src="/images/icon-complete.svg" alt="checkmark" />
       <h3>Thank you!</h3>
       <p>We've added your card details.</p>
-      <button type="submit">Continue</button>
+      <button type="submit" onClick={() => window.location.reload()}>
+        Continue
+      </button>
     </div>
   );
 }
